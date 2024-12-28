c553e0248c7fdbdb37ea4dcdf296baa2f55e36f6
Anastasia <158266030+Anastasiya-Stavrova@users.noreply.github.com>
2024-12-28 11:32:15 +0700

    completed LoginForm

---
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -0,0 +1,3 @@
+const FeedbackForm = () => {};
+
+export default FeedbackForm;
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Input, PasswordInput } from "@mantine/core";
+import { Input, PasswordInput, Space } from "@mantine/core";
 import * as yup from "yup";
 import { ILogin } from "../../types/login.ts";
 import getData from "../../utils/getData.ts";
@@ -36,6 +36,8 @@ const LoginForm = () => {
     reset();
   };
 
+  const space = "sm";
+
   return (
     <div className={styles.modal}>
       <form
@@ -43,46 +45,52 @@ const LoginForm = () => {
         onSubmit={handleSubmit(onSubmit)}
       >
         <h1 className={styles["modal-title"]}>Welcome!</h1>
-        <p className={styles["modal-desc"]}>
-          Login here using your email and password.
-        </p>
-        <div className="component">
-          <Input.Wrapper
-            className={styles.wrapper}
-            withAsterisk
-            label="EMAIL"
-            error={errors.email ? errors.email.message : ""}
-          >
-            <Input
-              className={`${errors.email ? "error" : ""} login-input-container`}
-              placeholder="Your email"
-              {...register("email")}
-              rightSection={<IconAt size={16} />}
-            />
-          </Input.Wrapper>
-        </div>
-        <div className="component">
-          <Input.Wrapper
-            className={styles.wrapper}
-            withAsterisk
-            label="PASSWORD"
-            error={errors.password ? errors.password.message : ""}
-          >
-            <PasswordInput
-              className={`${
-                errors.password ? "error" : ""
-              } login-input-container`}
-              placeholder="Your password"
-              {...register("password")}
-            />
-          </Input.Wrapper>
-        </div>
+        <p>Login here using your email and password.</p>
+
+        <Space h={space} />
+
+        <Input.Wrapper
+          className={styles.wrapper}
+          withAsterisk
+          label="EMAIL"
+          error={errors.email ? errors.email.message : ""}
+        >
+          <Input
+            className={`${errors.email ? "error" : ""} login-input-container`}
+            placeholder="Your email"
+            {...register("email")}
+            rightSection={<IconAt size={16} />}
+          />
+        </Input.Wrapper>
+
+        <Space h={space} />
+
+        <Input.Wrapper
+          className={styles.wrapper}
+          withAsterisk
+          label="PASSWORD"
+          error={errors.password ? errors.password.message : ""}
+        >
+          <PasswordInput
+            className={`${
+              errors.password ? "error" : ""
+            } login-input-container`}
+            placeholder="Your password"
+            {...register("password")}
+          />
+        </Input.Wrapper>
+
+        <Space h={space} />
+
         <div className={styles["modal-buttons"]}>
           <a className={styles["password-link"]} href="#">
             Forgot your password?
           </a>
           <button className={styles["login-button"]}>Login</button>
         </div>
+
+        <Space h="xl" />
+
         <p className={styles["sign-up"]}>
           Don't have an account?&nbsp;
           <a className={styles["sign-up-link"]} href="#">
